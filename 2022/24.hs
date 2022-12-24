e31ef053193394d9726fdfd595931d51651aa403
nshepperd <nshepperd@gmail.com>
2022-12-24 19:07:34 +1100

    .

--- 2022/24.hs
+++ 2022/24.hs
@@ -1,28 +1,3 @@
-{-# Language ConstraintKinds #-}
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE DeriveTraversable #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-
-import           Control.Applicative
-import           Control.Concurrent
-import           Control.Concurrent.STM
-import           Control.Concurrent.STM.TQueue
-import           Control.Lens
-import           Control.Monad
-import           Control.Monad.Codensity
-import           Control.Monad.Trans
-import           Control.Monad.Trans.State
-import           Data.Bits
-import           Data.Char
-import           Data.Foldable
-import           Data.Function.Memoize
-import           Data.Int
-import           Data.List
-import           Data.List.Split (chunksOf)
 import           Data.Map (Map)
 import qualified Data.Map.Strict as Map
 import           Data.Monoid
@@ -30,25 +5,9 @@ import           Data.Sequence (Seq)
 import qualified Data.Sequence as Seq
 import           Data.Set (Set)
 import qualified Data.Set as Set
-import           Data.Text (Text)
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
-import           Data.Traversable
-import           Data.Vector (Vector)
-import qualified Data.Vector.Generic as V
-import qualified Data.Vector.Unboxed as U
-import           Data.Word
-import           Debug.Trace
-import qualified Ersatz as E
-import qualified Ersatz.Counting as E
-import qualified Ersatz.Solver.Minisat as E
-import           Linear.V2
-import           Linear.V3
 import           Linear
 import           System.IO
 import           System.IO.Unsafe
-import           Text.Parser.Char
-import           Text.Parser.Combinators hiding (count)
 
 import           AStar
 import           Util
