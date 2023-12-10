543f7d0eda752746e7c1e6ba8347578f292525b4
nshepperd <nshepperd@gmail.com>
2023-12-11 08:18:29 +1100

    idk

--- 2023/10.hs
+++ 2023/10.hs
@@ -1,25 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 import           Control.Applicative
-import           Control.Monad
 import           Control.Lens
+import           Control.Monad
 import           Data.Char
+import           Data.Foldable
 import           Data.List
-import           Data.String
-import           Data.Text (Text)
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
-import           System.IO.Unsafe
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import           Data.Set (Set)
 import qualified Data.Set as Set
+import           Data.String
+import           Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import           Data.Traversable
+import qualified Data.Vector as V
+import           Debug.Trace
+import           Linear.V2
+import           Numeric.Search.Range
+import           System.IO.Unsafe
 import           Text.Parser.Char
 import           Text.Parser.Combinators
 import qualified Text.Trifecta as Trifecta
-import           Debug.Trace
-import           Numeric.Search.Range
-import qualified Data.Vector as V
-import           Linear.V2
 
 import           Search
 import           Util
@@ -53,15 +56,6 @@ part1 input = maximum $ map snd $ bfsOn fst actions (const True) start
     start = head [(p,0) | (p,'S') <- Map.toList grid]
     actions (p,cost) = [(p + v, cost+1) | d <- sides (gridc p), d /= C, let v = dv d, elem (-v) (map dv $ sides $ gridc $ p + v)]
 
-draw :: Char -> Char
-draw 'J' = '┘'
-draw 'F' = '┌'
-draw '7' = '┐'
-draw 'L' = '└'
-draw '|' = '│'
-draw '-' = '─'
-draw c = c
-
 sides :: Char -> [Direction]
 sides 'J' = [N, W, C]
 sides 'L' = [N, E, C]
@@ -72,22 +66,32 @@ sides '-' = [W, C, E]
 sides 'S' = [N,E,S,W,C]
 sides '.' = []
 
+draw :: Char -> Char
+draw 'J' = '┘'
+draw 'F' = '┌'
+draw '7' = '┐'
+draw 'L' = '└'
+draw '|' = '│'
+draw '-' = '─'
+draw c = c
+
 part2 input = drawAnswer >> print (Set.size inside)
   where
     drawAnswer = putStr $ unlines [concat [
                                       (case () of
-                                          _ | Set.member (V2 x y) inside -> "x"
+                                          _ | Set.member (V2 x y) inside -> "\x1b[32m"++ [draw c] ++ "\x1b[m"
                                             | Set.member (V2 x y) loop -> [draw c]
-                                            | otherwise -> ".")
+                                            | otherwise -> "\x1b[31m" ++ [draw c] ++ "\x1b[m")
                                       | (x,c) <- zip [0..] line] | (y,line) <- zip [0..] input]
 
     inside = Set.difference (Map.keysSet grid) (outside <> loop)
-    outside = Set.fromList [p | mp <- bfsOn id actions (const True) (-1), let (p,dp) = untriple mp, dp == 0]
+    outside = Set.fromList [p | mp <- bfsOn id actions (const True) (-1), let (p,dp) = untriple mp]
       where
         triple p = 3 * p + 1
         untriple mp = (fmap (`div` 3) mp, fmap (\a -> mod a 3 - 1) mp)
         inbounds (V2 x y) = (-1) <= x && x <= maxX + 1 && (-1) <= y && y <= maxY + 1
         walls = Set.fromList [triple p + dv d | p <- Set.toList loop, d <- sides (gridc p)]
+        -- walls = Set.fromList [triple p + dv d | p <- Map.keys grid, d <- sides (gridc p)]
         allowed mp = inbounds (fst $ untriple mp) && not (Set.member mp walls)
         actions p = [p + d | d <- [V2 1 0, -V2 1 0, V2 0 1, -V2 0 1], allowed (p + d)]
 
@@ -99,3 +103,5 @@ part2 input = drawAnswer >> print (Set.size inside)
     loop = Set.fromList $ bfsOn id actions (const True) start
       where
         actions p = [p + v | d <- sides (gridc p), d /= C, let v = dv d, elem (-v) (map dv $ sides $ gridc $ p + v)]
+
+main = traverse_ part2 input
